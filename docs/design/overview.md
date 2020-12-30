e1449de66f6d004bc57963b15a005c7ff926f4a0
hungnguyentiki <hung.nguyen7@tiki.vn>
2020-12-30 17:12:57 +0700

    feat(SCP-594): Add color component

--- docs/design/overview.md
+++ docs/design/overview.md
@@ -1,8 +1,13 @@
 ---
 order: 0
-title: 
+title:
   vi-VN: Tổng quan về thiết kế
   en-US: Design Overview
 ---
 
 ## Một hai ba
+
+```__react
+import {Color} from '../../theme/template/Design';
+ReactDOM.render(<Color color={{ name: 'gray', count: 13 }} direction="horizontal" />, mountNode);
+```
---
+++ theme/template/Design/Color/index.jsx
@@ -0,0 +1,75 @@
+import React, {useState, useEffect, useRef} from 'react';
+import { Tooltip, Button } from 'antd';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+import data from './data';
+
+const Cat = ({value}) => <span>{value}</span>;
+const ColorItem = ({locale,value, text})=> { 
+  const [copied, setCopied] = useState(false);
+  const timeout = useRef(null);
+  const copyText= locale === 'vi-VN' ? 'Đã sao chép' : 'Copied';
+
+  useEffect(() => {
+    if (copied) {
+      if (timeout.current) {
+        clearTimeout(timeout.current)
+      }
+      timeout.current = setTimeout(() => {
+        setCopied(false);
+      }, 3000)
+    }
+  }, [copied])
+
+
+
+  return <Tooltip placement="topLeft" title={value}>
+    <CopyToClipboard text={value} onCopy={() => setCopied(true)}>
+      <Button style={{alignItems: 'center',display: 'flex'}}>
+        <span style={{width: 12, height: 12, borderRadius: 6, backgroundColor:value,marginRight: 4 }}></span>
+        {copied ? copyText : text}
+    </Button>
+  </CopyToClipboard>
+</Tooltip>;
+}
+const Color = ({locale = 'vi-VN'}) => {
+  const localeData = data[locale];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {localeData.headers.map((item) => (
+            <td key={item}>{item}</td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {localeData.contents.map((item, index) => (
+          <tr key={index}>
+            {localeData.headers.map((_, colIndex) => {
+              let component = <></>;
+              const colValue = item[colIndex];
+              if (!colValue) {
+                return <td></td>;
+              }
+
+              if (colValue.type === 'cat') {
+                component = <Cat value={colValue.value} />
+              } else if (colValue.type === 'color') {
+                component = <ColorItem locale={locale} {...colValue}/>
+              } else {
+                component = <span>{colValue.value}</span>
+              }
+
+              return <td key={colIndex}>{component}</td>;
+              }
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Color;
