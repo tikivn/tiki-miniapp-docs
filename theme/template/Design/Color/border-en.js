020891ce3413faa1c1d6c5725830560e6d48f769
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-03-19 15:40:32 +0700

    fix design color

---
+++ theme/template/Design/Color/border-en.js
@@ -0,0 +1,197 @@
+export default {
+  headers: ['Key path', 'Value'],
+  contents: [
+    [
+      {
+        type: 'color',
+        text: 'border-button-secondary-overlay-hover',
+        value: '#FFFFFF',
+      },
+      {
+        type: 'color',
+        text: 'white 32%',
+        value: '#FFFFFF',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-button-secondary-overlay-active',
+        value: '#FFFFFF',
+      },
+      {
+        type: 'color',
+        text: 'white 54%',
+        value: '#FFFFFF',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-button-secondary-disable',
+        value: '#EBEBF0',
+      },
+      {
+        type: 'color',
+        text: 'gray20',
+        value: '#EBEBF0',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-button-secondary-disable',
+        value: '#EBEBF0',
+      },
+      {
+        type: 'color',
+        text: 'gray20',
+        value: '#EBEBF0',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-input-disable',
+        value: '#EBEBF0',
+      },
+      {
+        type: 'color',
+        text: 'gray20',
+        value: '#EBEBF0',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-selection-disable',
+        value: '#EBEBF0',
+      },
+      {
+        type: 'color',
+        text: 'gray20',
+        value: '#EBEBF0',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-tab-inactive',
+        value: '#EBEBF0',
+      },
+      {
+        type: 'color',
+        text: 'gray20',
+        value: '#EBEBF0',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-input-default',
+        value: '#DDDDE3',
+      },
+      {
+        type: 'color',
+        text: 'gray30',
+        value: '#DDDDE3',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-selection-default',
+        value: '#DDDDE3',
+      },
+      {
+        type: 'color',
+        text: 'gray30',
+        value: '#DDDDE3',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-input-hover',
+        value: '#A6A6B0',
+      },
+      {
+        type: 'color',
+        text: 'gray50',
+        value: '#A6A6B0',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-input-active',
+        value: '#81818A',
+      },
+      {
+        type: 'color',
+        text: 'gray60',
+        value: '#81818A',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-navigation-light-background',
+        value: '#28282B',
+      },
+      {
+        type: 'color',
+        text: 'gray100 8%',
+        value: '#28282B',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-navigation-dark-background',
+        value: '#28282B',
+      },
+      {
+        type: 'color',
+        text: 'gray100 16%',
+        value: '#28282B',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-button-secondary-default',
+        value: '#1A94FF',
+      },
+      {
+        type: 'color',
+        text: 'brand',
+        value: '#1A94FF',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-selection-active',
+        value: '#1A94FF',
+      },
+      {
+        type: 'color',
+        text: 'brand',
+        value: '#1A94FF',
+      },
+    ],
+    [
+      {
+        type: 'color',
+        text: 'border-tab-active',
+        value: '#1A94FF',
+      },
+      {
+        type: 'color',
+        text: 'brand',
+        value: '#1A94FF',
+      },
+    ],
+  ],
+};
