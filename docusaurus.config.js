8cf71752314d65c1782b1a6490afda1dc6379602
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-08-16 16:13:27 +0700

    feat: add update time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,11 +107,12 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/tikivn/tiki-miniapp-docs/blob/master',
+          showLastUpdateTime: true,
+          // editUrl: 'https://github.com/tikivn/tiki-miniapp-docs/blob/master',
         },
         blog: {
           showReadingTime: true,
-          editUrl: 'https://github.com/tikivn/tiki-miniapp-docs/blob/master',
+          // editUrl: 'https://github.com/tikivn/tiki-miniapp-docs/blob/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
