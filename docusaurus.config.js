8627d9b7f252bd1af39e1d7382e7c4c3a39ac8f7
hungnguyentiki <hung.nguyen7@tiki.vn>
2022-01-05 09:39:47 +0700

    fix: package version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -118,15 +118,6 @@ module.exports = {
       },
     ],
     path.resolve(__dirname, './plugins/docusaurus-plugin-facebook-pixel'),
-    [
-      '@docusaurus/plugin-sitemap',
-      {
-        id: 'tiniapp-sitemap',
-        changefreq: 'daily',
-        priority: 0.5,
-        trailingSlash: false,
-      },
-    ],
   ],
   presets: [
     [
@@ -145,6 +136,9 @@ module.exports = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
+        },
+        sitemap: {
+          changefreq: 'daily',
         },
       },
     ],
