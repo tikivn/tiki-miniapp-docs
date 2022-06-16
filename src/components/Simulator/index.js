b6fafd8b997a8248fcb5b47700b46ab32679e4f9
tri.nguyen1 <tri.nguyen1@tiki.vn>
2022-06-16 15:17:16 +0700

    fix URLSearchParams when building

--- src/components/Simulator/index.js
+++ src/components/Simulator/index.js
@@ -3,14 +3,17 @@ import React from 'react';
 const BASE_URL = 'https://beta.tiki.vn/apps';
 const DEFAULT_APP_ID = 'vn.tiki.miniapp.demo';
 
-const generateURL = (appId = '', page = '', params) => {
+const generateURL = (appId = '', page = '', params = {}) => {
   const fullParams = {
     ...params,
     simulateMobile: true,
   };
-  return `${BASE_URL}/${appId}/${page}?${new URLSearchParams(
-    fullParams,
-  ).toString()}`;
+
+  return `${BASE_URL}/${appId}/${page}?${
+    typeof window !== 'undefined'
+      ? new window.URLSearchParams(fullParams).toString()
+      : ''
+  }`;
 };
 
 export const Simulator = ({
